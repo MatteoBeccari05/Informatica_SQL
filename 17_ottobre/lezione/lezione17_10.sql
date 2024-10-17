d39cf2abc63866d72d32fac15a9d3d36717fb938
Matteo Beccari <98811589+MatteoBeccari05@users.noreply.github.com>
2024-10-17 12:08:12 +0200

    lezione 17 ottobre con interrogazioni

--- 17_ottobre/lezione/lezione17_10.sql
+++ 17_ottobre/lezione/lezione17_10.sql
@@ -20,10 +20,23 @@ from amministrazione.personale p
 where p.data_assunzione  between "1990-03-03" and "1993-04-26";
 
 -- quando si usa la group by non posso usare la clausola where dopo la group by
-select p.fascia_stipendio, (count(*)
+select p.fascia_stipendio, count(*)
 from amministrazione.personale p 
 where p.nome like "a%"
 group by p.fascia_stipendio
 having  p.fascia_stipendio in (1,3,5);
+
+
+
+-- SUM
+-- quante timbrature fa ogni persona 
+select t.codice_fiscale, count(*)
+from amministrazione.test t
+group by t.codice_fiscale ;
+
+
+select t.codice_fiscale, t.ingresso , t.uscita , sum(hour(timediff(t.uscita, t.ingresso)) * 60  + minute (timediff(t.uscita, t.ingresso))) as minuti_lavorati
+from amministrazione.test t
+group by t.codice_fiscale ;
 
 
